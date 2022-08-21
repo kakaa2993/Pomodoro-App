eb3c0a3e6fa11998f29e4b0005980308f4ac5101
kakaa2993@ <kakaa2993@gmail.com>
2022-08-21 08:30:16 -0700

    organise the code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+
 FONT = 'Time new roman'
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,17 +13,14 @@ LONG_BREAK_MIN = 20
 # CREATE THE WINDOWS
 windows = Tk()
 windows.title(string='Pomodoro APP')
-windows.minsize(width=600,height=500)
+windows.config(padx=100, pady=50, background=YELLOW)
+# windows.minsize(width=600,height=500)
 
 # Create the tomato image
-
-
-
-
-
-
-
-
-
+canvas = Canvas(width=200, height=223, background=YELLOW, highlightthickness=0)
+tomato_image = PhotoImage(file='tomato.png')
+canvas.create_image(100, 112, image=tomato_image)
+canvas.create_text(102, 135, text="00:00", font=(FONT_NAME, 30, "bold"), fill="white")
+canvas.pack()
 
 windows.mainloop()
