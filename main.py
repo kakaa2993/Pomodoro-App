0b023ce805e4e4e95f9c0070ad4f06afd4f60138
kakaa2993@ <kakaa2993@gmail.com>
2022-08-21 07:18:14 -0700

    add constances to the code

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+FONT = 'Time new roman'
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# CREATE THE WINDOWS
+windows = Tk()
+windows.title(string='Pomodoro APP')
+windows.minsize(width=600,height=500)
+
+# Create the tomato image
+
+
+
+
+
+
+
+
+
+
+windows.mainloop()
