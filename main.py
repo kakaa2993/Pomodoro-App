2da3244819c2080ff64f929c1f3940f53d7e42ff
kakaa2993@ <kakaa2993@gmail.com>
2022-08-23 00:06:09 -0700

    add the reps functionality

--- main.py
+++ main.py
@@ -8,30 +8,51 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 1
-SHORT_BREAK_MIN = 5
+SHORT_BREAK_MIN = 2
 LONG_BREAK_MIN = 20
-
+rep = 0
 # CREATE THE WINDOWS
 windows = Tk()
 windows.title(string='Pomodoro APP')
 windows.config(padx=100, pady=50, background=YELLOW)
 
-
-# windows.minsize(width=600,height=500)
+# 25 , sort break ,25 , short break,25 , short break,25 , long break
+# short break 1, 3, 5
+# work 0,2,4,6
+# long break 7
 
 
 def start_timer():
-    count = 5
-    count_down(count * 60)
+    global rep
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if rep == 0:
+        count_down(work_sec)
+        sessions.append("✔")
+        check_marks.config(text=f"{' '.join(sessions)}")
+    elif rep % 2 == 0:
+        count_down(work_sec)
+        sessions.append("✔")
+        check_marks.config(text=f"{' '.join(sessions)}")
+    elif rep % 7 == 0:
+        count_down(long_break_sec)
+    elif rep % 2 == 1:
+        count_down(short_break_sec)
+    print(rep)
+    rep += 1
 
 
 def count_down(count):
+    global rep
     count_min = math.floor(count / 60)
-    print(count_min)
     count_sec = count % 60
     if count > 0:
         windows.after(1000, count_down, count - 1)
     canvas.itemconfig(start_time, text=f"{count_min:02d}:{count_sec:02d}")
+    if count == 0 and rep != 7:
+        start_timer()
+
 
 
 # Create the tomato image
